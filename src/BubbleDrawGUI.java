2ec15b30dc6d921dfcde0a93c047149105708378
KamilJava <kamillapka1@gmail.com>
2019-03-08 20:29:49 +0100

    first commit

--- src/BubbleDrawGUI.java
+++ src/BubbleDrawGUI.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 public class BubbleDrawGUI extends JFrame {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("BubbleDraw App GUI");
+        JFrame frame = new JFrame("BubbleDrawGUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(new BubblePanel());
         frame.setSize(new java.awt.Dimension(1000,800));
